0fcfa837a9c6994a12003771174e0cf75491fc6b
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 20:48:22 +0600

    Add AllCurrencies cell

---
+++ Currencies/Currencies/Features/AllCurrencies/Cell/AllCurrenciesCell.swift
@@ -0,0 +1,16 @@
+//
+//  AllCurrenciesCell.swift
+//  Currencies
+//
+//  Created by Daniyar Erkinov on 29/4/21.
+//
+
+import Foundation
+class AllCurrenciesCell: TableViewCell<AllCurrenciesCellContentView> {
+    func configure(model: AllCurrencies.CurrencyDTO) {
+        mainContentView.titleLabel.text = model.title
+        
+        mainContentView.actionButton.setTitle(model.buttonState.buttonTitle, for: .normal)
+        mainContentView.actionButton.backgroundColor = model.buttonState.buttonBackgroundColor
+    }
+}
