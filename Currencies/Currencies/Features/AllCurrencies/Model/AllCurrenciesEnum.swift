46529aac8f15465e29219d33c3f002a7b7241023
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 20:41:12 +0600

    Add AllCurrencies enum model

---
+++ Currencies/Currencies/Features/AllCurrencies/Model/AllCurrenciesEnum.swift
@@ -0,0 +1,15 @@
+//
+//  AllCurrenciesEnum.swift
+//  Currencies
+//
+//  Created by Daniyar Erkinov on 29/4/21.
+//
+
+import Foundation
+
+enum AllCurrencies {
+    struct CurrencyDTO {
+        let title: String
+        let isAdded: Bool
+    }
+}
