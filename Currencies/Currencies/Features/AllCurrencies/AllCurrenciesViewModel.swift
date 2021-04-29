be0bfa06d4376e9cb509937fb97c48fa67b31318
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 20:52:48 +0600

    Add required properties in AllCurrenciesViewModel

--- Currencies/Currencies/Features/AllCurrencies/AllCurrenciesViewModel.swift
+++ Currencies/Currencies/Features/AllCurrencies/AllCurrenciesViewModel.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 class AllCurrenciesViewModel: BaseViewModel {
+    enum SearchMode {
+        case normal
+        case search
+    }
     
+    private var searchMode: SearchMode = .normal
+    
+    private var items: [AllCurrencies.CurrencyDTO] = []
+    private var searchItems: [AllCurrencies.CurrencyDTO] = []
+    
+    var resultItems: [AllCurrencies.CurrencyDTO] {
+        switch searchMode {
+        case .normal:
+            return items
+        case .search:
+            return searchItems
+        }
+    }
 }
-
