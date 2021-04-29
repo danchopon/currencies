b90327da55c6159ebf7d8304f3a87226b8c61dc4
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 20:03:06 +0600

    Add BaseViewController

---
+++ Currencies/Currencies/Common/Components/Base/BaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.swift
+//  Currencies
+//
+//  Created by Daniyar Erkinov on 29/4/21.
+//
+
+import UIKit
+
+class BaseViewController<ContentView: BaseContentView, ViewModel: BaseViewModel>: UIViewController, ViewControlable {
+    let contentView: ContentView
+    let viewModel: ViewModel
+    
+    init(contentView: ContentView, viewModel: ViewModel) {
+        self.contentView = contentView
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel.baseDelegate = self
+        contentView.setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BaseViewController: BaseViewModelDelegate {
+    func viewModel(_ viewModel: ViewModelable, didUpdateLoadingState state: LoadingState) {
+        
+    }
+}
