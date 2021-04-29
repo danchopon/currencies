f3a9c8cfbcb65ae1d2a57634226910b8a9605e07
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 19:23:15 +0600

    Add AllCurrencies Coordinator

---
+++ Currencies/Currencies/Navigation/Coordinators/AllCurrencies/AllCurrenciesCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  AllCurrenciesCoordinator.swift
+//  Currencies
+//
+//  Created by Daniyar Erkinov on 29/4/21.
+//
+
+import UIKit
+
+final class AllCurrenciesCoordinator: BaseCoordinator {
+    private let router: Router
+    private let factory: AllCurrenciesFactory
+    private let services: Services
+    
+    init(router: Router, factory: AllCurrenciesFactory, services: Services) {
+        self.router = router
+        self.factory = factory
+        self.services = services
+    }
+    
+    override func start() {
+        showAllCurrencies()
+    }
+    
+    func showAllCurrencies() {
+        let vc = factory.makeAllCurrenciesVC()
+        router.push(vc)
+    }
+}
