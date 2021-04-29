220c6a0e9ebbfe57600ef34a6f577967e6a35813
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 19:51:26 +0600

    Change UITabBarItem extension's image rendering mode in convenience init

--- Currencies/Currencies/Common/Extensions/UITabBarItem+Extensions.swift
+++ Currencies/Currencies/Common/Extensions/UITabBarItem+Extensions.swift
@@ -11,8 +11,8 @@ extension UITabBarItem {
     convenience init(title: String, image: UIImage, selectedImage: UIImage, tag: Int) {
         self.init()
         self.title = title
-        self.image = image.withRenderingMode(.alwaysOriginal)
-        self.selectedImage = selectedImage.withRenderingMode(.alwaysOriginal)
+        self.image = image.withRenderingMode(.alwaysTemplate)
+        self.selectedImage = selectedImage.withRenderingMode(.alwaysTemplate)
         self.tag = tag
     }
 }
