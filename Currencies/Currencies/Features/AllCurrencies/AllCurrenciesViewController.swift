53c8973d02b0c07b3311078248af08bb69609866
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 21:52:40 +0600

    Setup table view in AllCurrencies ViewController

--- Currencies/Currencies/Features/AllCurrencies/AllCurrenciesViewController.swift
+++ Currencies/Currencies/Features/AllCurrencies/AllCurrenciesViewController.swift
@@ -10,5 +10,28 @@ import UIKit
 class AllCurrenciesViewController: BaseViewController<AllCurrenciesContentView, AllCurrenciesViewModel> {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
     }
+    
+    private func setupTableView() {
+        contentView.tableView.dataSource = self
+        contentView.tableView.delegate = self
+    }
+}
+
+extension AllCurrenciesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = viewModel.item(at: indexPath.row)
+        let cell: AllCurrenciesCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.configure(model: item)
+        return cell
+    }
+}
+
+extension AllCurrenciesViewController: UITableViewDelegate {
+    
 }
