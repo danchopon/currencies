716945b6760e162a7b9a5521024ab6692741bd13
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-29 21:50:12 +0600

    Setup AllCurrencies ContentView

--- Currencies/Currencies/Features/AllCurrencies/AllCurrenciesContentView.swift
+++ Currencies/Currencies/Features/AllCurrencies/AllCurrenciesContentView.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class AllCurrenciesContentView: UIView {
+    lazy var refreshControl = makeRefreshControl()
+    lazy var tableView = makeTableView()
+    lazy var emptyPlaceholderView = makeEmptyPlaceholderView()
+    
     init() {
         super.init(frame: .zero)
         setSubviews()
@@ -21,14 +25,36 @@ class AllCurrenciesContentView: UIView {
 
 extension AllCurrenciesContentView: BaseContentView {
     public func setSubviews() {
-        
+        addSubview(tableView)
     }
     
     public func setConstraints() {
-        
+        tableView.fillSuperview()
+        emptyPlaceholderView.anchor(
+            .centerX(tableView.centerXAnchor),
+            .centerY(tableView.centerYAnchor),
+            .width(tableView.frame.width),
+            .height(tableView.frame.height)
+        )
     }
 }
 
 private extension AllCurrenciesContentView {
+    func makeRefreshControl() -> RefreshControl {
+        return RefreshControl(tintColor: .gray)
+    }
     
+    func makeEmptyPlaceholderView() -> EmptyPlaceholderView {
+        return EmptyPlaceholderView()
+    }
+    
+    func makeTableView() -> TableView {
+        return TableView(
+            refreshControl: refreshControl,
+            cells: [AllCurrenciesCell.self],
+            automaticDimentioned: true,
+            contentInset: UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0),
+            emptyPlaceholderView: emptyPlaceholderView
+        )
+    }
 }
