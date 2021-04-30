37877bf17af963b9b4e7ae41c1fbc0bd8624fa4a
Daniiar Erkinov <dan4111k1@gmail.com>
2021-04-30 23:58:32 +0600

    Setup cell button targets and delegate

--- Currencies/Currencies/Features/AllCurrencies/Cell/AllCurrenciesCell.swift
+++ Currencies/Currencies/Features/AllCurrencies/Cell/AllCurrenciesCell.swift
@@ -6,11 +6,38 @@
 //
 
 import Foundation
+
+protocol AllCurrenciesCellDelegate: AnyObject {
+    func allCurrenciesCellDidTapAddButton(_ cell: AllCurrenciesCell, onIndexPath indexPath: IndexPath?)
+    func allCurrenciesCellDidTapRemoveButton(_ cell: AllCurrenciesCell, onIndexPath indexPath: IndexPath?)
+}
+
 class AllCurrenciesCell: TableViewCell<AllCurrenciesCellContentView> {
+    weak var delegate: AllCurrenciesCellDelegate?
+    
+    var indexPath: IndexPath?
+    
     func configure(model: AllCurrencies.CurrencyDTO) {
         mainContentView.titleLabel.text = model.title
         
         mainContentView.actionButton.setTitle(model.buttonState.title, for: .normal)
         mainContentView.actionButton.backgroundColor = model.buttonState.backgroundColor
+        
+        switch model.buttonState {
+        case .add:
+            mainContentView.actionButton.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
+        case .remove:
+            mainContentView.actionButton.addTarget(self, action: #selector(didTapRemoveButton), for: .touchUpInside)
+        }
+    }
+}
+
+@objc private extension AllCurrenciesCell {
+    func didTapAddButton() {
+        delegate?.allCurrenciesCellDidTapAddButton(self, onIndexPath: indexPath)
+    }
+    
+    func didTapRemoveButton() {
+        delegate?.allCurrenciesCellDidTapRemoveButton(self, onIndexPath: indexPath)
     }
 }
